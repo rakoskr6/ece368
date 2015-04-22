32dc8bdec807a53e33dc74ace3911070d176c380
rakoskr6 <krakos@purdue.edu>
2015-04-22 14:21:58 -0400

    Added text file

--- proj3/project3.c
+++ proj3/project3.c
@@ -2,7 +2,22 @@
 #include <stdio.h>
 #include <math.h>
 
-int main(void)
-{
-
+int main(int argc, char * * argv)
+{	
+	if (argc != 2)
+	{
+		printf("Error, need input file\n");
+		return EXIT_FAILURE;
+	}
+	
+	FILE	 *FID = fopen(argv[1],"r");
+	if (FID == NULL)
+	{
+		printf("Need valid file name\n");
+		return EXIT_FAILURE;
+	}
+	
+	
+	fclose(FID);
+	return EXIT_SUCCESS;
 }
